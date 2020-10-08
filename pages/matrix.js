20e4acee64cfae832f86ca4db7a78e8361bbc4c0
Lawlez <dk.feger@gmail.com>
2020-10-08 19:30:44 +0200

    Update matrix.js

--- pages/matrix.js
+++ pages/matrix.js
@@ -38,13 +38,13 @@ const MatrixEffect = ({ children }) => {
   // Setting up the draw function
   const draw = () => {
     ctx.fillStyle = "rgba(0, 0, 0, 0.08)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
     for (let i = 0; i < drops.length; i++) {
       let text = letters[Math.floor(Math.random() * letters.length)];
       ctx.fillStyle = "#0f0";
-      ctx.fillText(text, i * fontSize, drops[i] * fontSize - 2);
+      ctx.fillText(text, i * fontSize, drops[i] * fontSize);
       drops[i]++;
-      if (drops[i] * fontSize > canvas.height && Math.random() > 0.98) {
+      if (drops[i] * fontSize > window.innerHeight && Math.random() > 0.99) {
         drops[i] = 0;
       }
     }
